d8e5fca73bf105f7249569891e1539ae0a0010e0
RedCMD <theredcmd@gmail.com>
2022-08-17 19:31:36 +1200

    -

--- grammar.js
+++ grammar.js
@@ -1,5 +1,5 @@
 module.exports = grammar({
-	name: "json-tm",
+	name: "jsontm",
 	extras: $ => [
 		$._whitespace,
 	],
@@ -42,56 +42,6 @@ module.exports = grammar({
 			/(\"|\\|\/|b|f|n|r|t|u)/
 		)),
 
-		number: $ => {
-			const hex_literal = seq(
-				choice('0x', '0X'),
-				/[\da-fA-F]+/
-			)
-
-			const decimal_digits = /\d+/
-			const signed_integer = seq(optional(choice('-', '+')), decimal_digits)
-			const exponent_part = seq(choice('e', 'E'), signed_integer)
-
-			const binary_literal = seq(choice('0b', '0B'), /[0-1]+/)
-
-			const octal_literal = seq(choice('0o', '0O'), /[0-7]+/)
-
-			const decimal_integer_literal = seq(
-				optional(choice('-', '+')),
-				choice(
-					'0',
-					seq(/[1-9]/, optional(decimal_digits))
-				)
-			)
-
-			const decimal_literal = choice(
-				seq(decimal_integer_literal, '.', optional(decimal_digits), optional(exponent_part)),
-				seq('.', decimal_digits, optional(exponent_part)),
-				seq(decimal_integer_literal, optional(exponent_part))
-			)
-
-			return token(choice(
-				hex_literal,
-				decimal_literal,
-				binary_literal,
-				octal_literal
-			))
-		},
-
-		true: $ => "true",
-
-		false: $ => "false",
-
-		null: $ => "null",
-
-		comment: $ => token(choice(
-			seq('//', /.*/),
-			seq(
-				'/*',
-				/[^*]*\*+([^/*][^*]*\*+)*/,
-				'/'
-			)
-		)),
 	}
 });
 
