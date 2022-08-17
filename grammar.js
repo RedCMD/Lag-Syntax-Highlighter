c7e7c8084337e605a241b6857a861938d1422828
RedCMD <theredcmd@gmail.com>
2022-08-17 19:58:50 +1200

    g

--- grammar.js
+++ grammar.js
@@ -25,11 +25,3 @@ module.exports = grammar({
 		_whitespace: $ => /[^\S\r\n]/,
 	}
 });
-
-function commaSep1(rule) {
-	return seq(rule, repeat(seq(",", rule)))
-}
-
-function commaSep(rule) {
-	return optional(commaSep1(rule))
-}
