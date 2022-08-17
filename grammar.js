225c608b0243cb44ed5a7dd5912cc10d1b48b83d
RedCMD <theredcmd@gmail.com>
2022-08-17 19:56:39 +1200

    d

--- grammar.js
+++ grammar.js
@@ -9,40 +9,18 @@ module.exports = grammar({
 			$.object,
 		),
 		
-		_value: $ => choice(
-			$.object,
-			$.array,
-			$.string
-		),
-
 		object: $ => seq(
-			"{", commaSep($.pair), "}"
+			'{',
+			seq(
+				$.version,
+				$.$schema,
+				$.scopeName,
+				$.name,
+				$.patterns,
+				$.repository,
+			),
+			'}',
 		),
-
-		pair: $ => seq(
-			field("key", $.string),
-			":",
-			field("value", $._value)
-		),
-
-		array: $ => seq(
-			"[", commaSep($._value), "]"
-		),
-
-		string: $ => choice(
-			seq('"', '"'),
-			seq('"', $.string_content, '"')
-		),
-
-		string_content: $ => repeat1(choice(
-			token.immediate(prec(1, /[^\\"\n]+/)),
-			$.escape_sequence
-		)),
-
-		escape_sequence: $ => token.immediate(seq(
-			'\\',
-			/(\"|\\|\/|b|f|n|r|t|u)/
-		)),
 		
 		_whitespace: $ => /[^\S\r\n]/,
 	}
